84f966307c0ba743eff4082c756d51acf58f111e
Mantri Tirth Satyanarayan <140224736+Tirthmantri20@users.noreply.github.com>
2026-03-08 15:07:41 +0530

    Final Axios implementation for book retrieval

--- final_project/router/general.js
+++ final_project/router/general.js
@@ -1,10 +1,12 @@
-const express = require("express");
+const express = require('express');
 const axios = require('axios');
 let books = require("./booksdb.js");
 let isValid = require("./auth_users.js").isValid;
 let users = require("./auth_users.js").users;
+
 const public_users = express.Router();
 
+/* Register new user */
 public_users.post("/register", (req,res) => {
 
   const username = req.body.username;
@@ -13,50 +15,42 @@ public_users.post("/register", (req,res) => {
   if(username && password){
 
     if(!isValid(username)){
-      users.push({"username": username, "password": password});
-      return res.status(200).json({message: "User successfully registered. Now you can login"});
-    } 
-    else {
-      return res.status(404).json({message: "User already exists!"});
+      users.push({"username":username,"password":password});
+      return res.status(200).json({message:"User successfully registered. Now you can login"});
+    } else {
+      return res.status(404).json({message:"User already exists!"});
     }
 
   }
 
-  return res.status(404).json({message: "Unable to register user."});
+  return res.status(404).json({message:"Unable to register user."});
 });
 
 
-
-// Get the book list available in the shop
-public_users.get('/asyncbooks', async function (req, res) {
-
-  try {
-    const response = await axios.get("http://localhost:5001/books");
-    return res.status(200).json(response.data);
-  } catch (error) {
-    return res.status(500).json({message: "Error retrieving books"});
-  }
-
+/* Get the book list available in the shop */
+public_users.get('/books',function (req, res) {
+  return res.status(200).json(books);
 });
 
-// Get book details based on ISBN
-public_users.get('/asyncisbn/:isbn', function (req, res) {
+
+/* Get book details based on ISBN (Promise with Axios) */
+public_users.get('/isbn/:isbn',function (req, res) {
 
   const isbn = req.params.isbn;
 
   axios.get("http://localhost:5001/books")
   .then(response => {
-    const book = response.data[isbn];
-    res.status(200).json(book);
+      res.status(200).json(response.data[isbn]);
   })
-  .catch(err => {
-    res.status(500).json({message: "Error retrieving book"});
+  .catch(error => {
+      res.status(500).json({message:"Error retrieving book"});
   });
 
 });
 
-// Get book details based on author
-public_users.get('/asyncauthor/:author', async function (req, res) {
+
+/* Get book details based on Author (Async/Await with Axios) */
+public_users.get('/author/:author', async function (req, res) {
 
   const author = req.params.author;
 
@@ -68,21 +62,22 @@ public_users.get('/asyncauthor/:author', async function (req, res) {
     let result = {};
 
     Object.keys(booksData).forEach(key => {
-      if (booksData[key].author === author) {
+      if(booksData[key].author === author){
         result[key] = booksData[key];
       }
     });
 
-    return res.status(200).json(result);
+    res.status(200).json(result);
 
-  } catch(error) {
-    return res.status(500).json({message: "Error retrieving books"});
+  } catch(error){
+    res.status(500).json({message:"Error retrieving books"});
   }
 
 });
 
-// Get all books based on title
-public_users.get('/asynctitle/:title', function (req, res) {
+
+/* Get all books based on Title (Promise with Axios) */
+public_users.get('/title/:title',function (req, res) {
 
   const title = req.params.title;
 
@@ -93,7 +88,7 @@ public_users.get('/asynctitle/:title', function (req, res) {
     let result = {};
 
     Object.keys(booksData).forEach(key => {
-      if (booksData[key].title === title) {
+      if(booksData[key].title === title){
         result[key] = booksData[key];
       }
     });
@@ -102,17 +97,19 @@ public_users.get('/asynctitle/:title', function (req, res) {
 
   })
   .catch(error => {
-    res.status(500).json({message: "Error retrieving books"});
+    res.status(500).json({message:"Error retrieving books"});
   });
 
 });
 
-//  Get book review
-public_users.get('/review/:isbn', function (req, res) {
+
+/* Get book reviews */
+public_users.get('/review/:isbn',function (req, res) {
 
   const isbn = req.params.isbn;
-
   return res.status(200).json(books[isbn].reviews);
+
 });
+
 
 module.exports.general = public_users;
